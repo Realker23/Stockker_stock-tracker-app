a44c34a53c7a396d6295716e044418f48ab4b72e
arousha <harman.roushan@mercedes-benz.com>
2026-01-29 19:35:32 +0530

    Implement home page with TradingView widgets

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -30,9 +30,9 @@ const UserDropdown = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
+        <Button variant="ghost" className="flex items-center gap-3 text-gray-400 hover:text-yellow-500">
             <Avatar className="h-8 w-8">
-                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarImage src="https://github.com/shadcn.png" aria-hidden="true"/>
                 <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">{user.name[0]}</AvatarFallback>
             </Avatar>
             <div className="hidden md:flex flex-col items-start">
@@ -44,7 +44,7 @@ const UserDropdown = () => {
         <DropdownMenuLabel>
             <div className="flex relative items-center gap-3 py-2">
              <Avatar className="h-10 w-10">
-                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarImage src="https://github.com/shadcn.png" aria-hidden="true"/>
                 <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">{user.name[0]}</AvatarFallback>
             </Avatar>
             <div className="flex flex-col">
@@ -54,7 +54,7 @@ const UserDropdown = () => {
         </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator className="bg-gray-600"/>
-        <DropdownMenuItem onClick={handleSignOut} className="test-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
+        <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
             <LogOut className="h-4 w-4 mr-2 hidden sm:block"/>
             Logout
         </DropdownMenuItem>
